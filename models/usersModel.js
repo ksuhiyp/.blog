f48d62d8acc484ae87ebf25e8a0766418b80568c
suhaib hassan kharabsheh <ksuhiyp@gmail.com>
2018-06-14 01:09:01 +0300

    User Model Schema and REST work

---
+++ models/usersModel.js
@@ -0,0 +1,54 @@
+/**
+Users can be Admins || Authors || Editors || *Visitors by design under @role attr
+Admins only can grant access to this model
+
+*/
+const mongoose = require('mongoose');
+const userModel = mongoose.Schema({
+  userName: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  middleName: {
+    type: String,
+    required: false
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  hash: {
+    type: String,
+    required: true
+  },
+  profilePicture: {
+    type: String,
+    required: flase
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  twitter: {
+    type: String,
+    required: false
+  },
+  country: {
+    type: String,
+    required: flase
+  },
+  language: {
+    type: String,
+    required: true
+  },
+  mobile: {
+    type: String,
+    required: false
+  }
+
+})
+//TODO: make REST work
